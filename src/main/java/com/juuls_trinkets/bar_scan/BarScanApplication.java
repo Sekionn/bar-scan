50fa94e2b37c28b79b3baa4068a423a5c2782937
Sekionn <Sjknudesn@gmail.com>
2025-11-05 19:40:07 +0100

    indent mistake fixed

--- src/main/java/com/juuls_trinkets/bar_scan/BarScanApplication.java
+++ src/main/java/com/juuls_trinkets/bar_scan/BarScanApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class BarScanApplication {
 
-@RequestMapping("/")
+        @RequestMapping("/")
         public String home() {
                 return "Hello World!";
         }
